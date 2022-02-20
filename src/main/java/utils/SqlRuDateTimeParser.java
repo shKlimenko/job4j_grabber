368df3e8873cc38489a8a56d947e3fafe5b5deff
Master <klimenko161@gmail.com>
2022-02-20 22:10:08 +0300

    upd

---
+++ src/main/java/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    /* private static final Map<String, String> MONTHS = Map.ofEntries(...); */
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        return null;
+    }
+}
