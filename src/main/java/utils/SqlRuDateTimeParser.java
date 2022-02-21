ffd7efa71b938cc9b030615a25969c050171c058
PK rabota <klimenko161@gmail.com>
2022-02-21 13:56:05 +0300

    upd

--- src/main/java/utils/SqlRuDateTimeParser.java
+++ src/main/java/utils/SqlRuDateTimeParser.java
@@ -1,14 +1,45 @@
 package utils;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
-    /* private static final Map<String, String> MONTHS = Map.ofEntries(...); */
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "01"),
+            Map.entry("фев", "02"),
+            Map.entry("мар", "03"),
+            Map.entry("апр", "04"),
+            Map.entry("май", "05"),
+            Map.entry("июн", "06"),
+            Map.entry("июл", "07"),
+            Map.entry("авг", "08"),
+            Map.entry("сен", "09"),
+            Map.entry("окт", "10"),
+            Map.entry("ноя", "11"),
+            Map.entry("дек", "12")
+    );
+
+    public static void main(String[] args) {
+        SqlRuDateTimeParser srdtp = new SqlRuDateTimeParser();
+        System.out.println(srdtp.parse("15 дек 22, 22:15"));
+    }
 
     @Override
     public LocalDateTime parse(String parse) {
-        return null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MM yy, HH:mm");
+        LocalDateTime date;
+        if (parse.toLowerCase().contains("сегодня")) {
+            date = LocalDateTime.now().withNano(0);
+        } else if (parse.toLowerCase().contains("вчера")) {
+            date = LocalDateTime.now().minusDays(1).withNano(0);
+        } else {
+            String[] strings = parse.split(" ");
+            parse = parse.replace(strings[1], MONTHS.get(strings[1]));
+            date = LocalDateTime.parse(parse, formatter);
+        }
+        return date;
     }
 }
