df9be13aa28c290ac4ab1630ce8b80716dbe3d7f
Master <klimenko161@gmail.com>
2022-02-22 22:38:52 +0300

    2.3. Загрузка деталей поста. [#285212]

---
+++ src/main/java/html/ForumPageParse.java
@@ -0,0 +1,24 @@
+package html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import post.Post;
+import utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+
+public class ForumPageParse {
+    public static Post pageParse(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        Elements msgBody = doc.select(".msgBody");
+        String msgFooter = msgBody.get(0).parent().parent().child(2).text();
+        String strDate = msgFooter.substring(0, msgFooter.indexOf(" ["));
+        return new Post(
+                msgBody.get(0).parent().parent().child(0).text(),
+                msgBody.get(0).child(0).attr("href"),
+                msgBody.get(1).text(),
+                new SqlRuDateTimeParser().parse(strDate)
+        );
+    }
+}
