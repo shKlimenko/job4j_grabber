b487c77ad37b44f733850193418916643baa4b85
Master <klimenko161@gmail.com>
2022-02-23 19:00:15 +0300

    4. Схема таблицы Post [#1731]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -2,3 +2,11 @@ create table if not exists rabbit (
     id serial primary key,
     created_date timestamp
 );
+
+create table if not exists post (
+    id serial primary key,
+    name text,
+    link text unique,
+    text text,
+    created_date timestamp
+);
