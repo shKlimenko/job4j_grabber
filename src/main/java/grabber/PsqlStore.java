4473445c6e0462953e5b7bb7d8cad11e8891577c
Master <klimenko161@gmail.com>
2022-03-09 07:32:48 +0300

    upd

--- src/main/java/grabber/PsqlStore.java
+++ src/main/java/grabber/PsqlStore.java
@@ -1,7 +1,6 @@
 package grabber;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.List;
 import java.util.Properties;
 
@@ -9,18 +8,30 @@ public class PsqlStore implements Store, AutoCloseable {
 
     private Connection cnn;
 
-    public PsqlStore(Properties cfg) {
+    public PsqlStore(Properties cfg) throws SQLException {
         try {
             Class.forName(cfg.getProperty("jdbc.driver"));
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
-        /* cnn = DriverManager.getConnection(...); */
+        cnn = DriverManager.getConnection(
+                cfg.getProperty("jdbc.url"),
+                cfg.getProperty("jdbc.login"),
+                cfg.getProperty("jdbc.password"));
     }
 
     @Override
     public void save(Post post) {
-
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "insert into post(name, link, text, created_date) values(?, ?, ?, ?)")) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getLink());
+            statement.setString(3, post.getDescription());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
