21e1149080022e906fb47f6a82b10aaff7ee25a5
Master <klimenko161@gmail.com>
2022-03-16 21:49:16 +0300

    5. PsqlStore [#285209]

--- src/main/java/grabber/PsqlStore.java
+++ src/main/java/grabber/PsqlStore.java
@@ -1,6 +1,8 @@
 package grabber;
 
+import java.io.InputStream;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -19,6 +21,33 @@ public class PsqlStore implements Store, AutoCloseable {
                 cfg.getProperty("jdbc.url"),
                 cfg.getProperty("jdbc.login"),
                 cfg.getProperty("jdbc.password"));
+    }
+
+    public static void main(String[] args) {
+        try (InputStream in = PsqlStore.class.getClassLoader()
+                .getResourceAsStream("jdbc.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            PsqlStore psqlStore = new PsqlStore(config);
+            psqlStore.save(new Post("name", "link", "description",
+                    LocalDateTime.now().withSecond(0).withNano(0)));
+            psqlStore.save(new Post("name2", "link2", "description2",
+                    LocalDateTime.now().withSecond(0).withNano(0)));
+            System.out.println(psqlStore.getAll());
+            System.out.println(psqlStore.findById(1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Post getPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("text"),
+                resultSet.getTimestamp("created_date").toLocalDateTime()
+        );
     }
 
     @Override
@@ -48,16 +77,6 @@ public class PsqlStore implements Store, AutoCloseable {
             e.printStackTrace();
         }
         return allPosts;
-    }
-
-    public Post getPost(ResultSet resultSet) throws SQLException {
-        return new Post(
-                resultSet.getInt("id"),
-                resultSet.getString("name"),
-                resultSet.getString("link"),
-                resultSet.getString("text"),
-                resultSet.getTimestamp("created_date").toLocalDateTime()
-        );
     }
 
     @Override
