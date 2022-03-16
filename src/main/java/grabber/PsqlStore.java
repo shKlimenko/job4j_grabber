417a28e2b6647fd5b275fca6c799a9f36ec82d11
Master <klimenko161@gmail.com>
2022-03-16 20:21:06 +0300

    upd

--- src/main/java/grabber/PsqlStore.java
+++ src/main/java/grabber/PsqlStore.java
@@ -1,6 +1,7 @@
 package grabber;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -36,12 +37,44 @@ public class PsqlStore implements Store, AutoCloseable {
 
     @Override
     public List<Post> getAll() {
-        return null;
+        List<Post> allPosts = new ArrayList<>();
+        try (PreparedStatement statement = cnn.prepareStatement("select * from post")) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    allPosts.add(getPost(resultSet));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return allPosts;
+    }
+
+    public Post getPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("description"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
     }
 
     @Override
     public Post findById(int id) {
-        return null;
+        Post post = null;
+        try (PreparedStatement statement = cnn.prepareStatement(
+                "select * from post where post.id = ?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = getPost(resultSet);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 
     @Override
