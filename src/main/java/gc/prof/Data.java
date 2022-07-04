f3e824c4f7e974c94f8f570e56dce8bd11a53390
Admin <klimenko161@gmail.com>
2022-07-04 19:03:52 +0300

    update

---
+++ src/main/java/gc/prof/Data.java
@@ -0,0 +1,7 @@
+package gc.prof;
+
+public interface Data {
+    void insert(int elements);
+
+    int[] getClone();
+}
---
+++ src/main/java/gc/prof/Sort.java
@@ -0,0 +1,5 @@
+package gc.prof;
+
+public interface Sort {
+    boolean sort(Data data);
+}
