a39176ba59d7e53bb951d512aeac6b05b9e8a284
Master <klimenko161@gmail.com>
2022-07-05 23:33:40 +0300

    1. Эксперименты с различными GC [#1590 #313586]

--- src/main/java/gc/prof/Go.java
+++ src/main/java/gc/prof/Go.java
@@ -1,14 +1,37 @@
 package gc.prof;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Go {
     public static void main(String[] args) {
         Data data = new RandomArray(new Random());
-        data.insert(250000);
-
-        new MergeSort().sort(data);
-        new InsertSort().sort(data);
-        new BubbleSort().sort(data);
+        String out = "no";
+        Scanner sc = new Scanner(System.in);
+        while (!out.equals("out")) {
+            int input = sc.nextInt();
+            switch (input) {
+                case 1:
+                    data.insert(250000);
+                    System.out.println("Insert elements");
+                    break;
+                case 2:
+                    new MergeSort().sort(data);
+                    System.out.println("MergeSort");
+                    break;
+                case 3:
+                    new InsertSort().sort(data);
+                    System.out.println("InsertSort");
+                    break;
+                case 4:
+                    new BubbleSort().sort(data);
+                    System.out.println("BubbleSort");
+                    break;
+                case 5:
+                    out = "out";
+                    System.out.println("out");
+                    break;
+            }
+        }
     }
 }
